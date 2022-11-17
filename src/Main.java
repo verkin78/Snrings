3c0e4bf8eac884cf12eba9bb08deb9c7d3f8ee67
Verkin78 <otrajenie.jc@gmail.com>
2022-11-17 16:03:06 +0300

    Задание3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        String firstname = "Иван";
-        String middlename = "Иванович";
+        String firstname = "Семён";
+        String middlename = "Семёнович";
         String lastlename = "Иванов";
         firstname = firstname + " ";
         lastlename = lastlename + " ";
         String fullname = lastlename + firstname + middlename;
+        fullname = fullname.replace ("ё", "е");
+
         System.out.println("ФИО сотрудника - " + fullname);
         fullname = fullname.toUpperCase();
         System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullname);
-
-
     }
 }
